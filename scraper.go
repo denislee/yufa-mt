d0904002ca9d2c87c4142551cf4b80627ffc5208
Denis Lee <denislee@gmail.com>
2025-11-03 12:01:33 -0300

    chore: disable immediately run jobs

--- scraper.go
+++ scraper.go
@@ -2752,7 +2752,7 @@ func runJobOnTicker(ctx context.Context, job Job) {
 	defer ticker.Stop()
 
 	log.Printf("[I] [Job] Starting initial run for %s job...", job.Name)
-	job.Func() // Run immediately on start
+	//job.Func() // Run immediately on start
 
 	for {
 		select {
