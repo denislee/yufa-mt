79d69d3f38f09fbb94d5055b9cf9a0ea18b75339
Denis Lee <denislee@gmail.com>
2025-11-03 14:44:39 -0300

    chore: changed intervals for scraping

--- scraper.go
+++ scraper.go
@@ -2771,12 +2771,12 @@ func startBackgroundJobs(ctx context.Context) {
 	jobs := []Job{
 		{Name: "Market", Func: scrapeData, Interval: 3 * time.Minute},
 		{Name: "Player Count", Func: scrapeAndStorePlayerCount, Interval: 1 * time.Minute},
-		{Name: "Player Character", Func: scrapePlayerCharacters, Interval: 1 * time.Hour},
-		{Name: "Guild", Func: scrapeGuilds, Interval: 25 * time.Minute},
-		{Name: "Zeny", Func: scrapeZeny, Interval: 1 * time.Hour},
+		{Name: "Player Character", Func: scrapePlayerCharacters, Interval: 6 * time.Hour},
+		{Name: "Guild", Func: scrapeGuilds, Interval: 1 * time.Hour},
+		{Name: "Zeny", Func: scrapeZeny, Interval: 6 * time.Hour},
 		{Name: "MVP Kill", Func: scrapeMvpKills, Interval: 5 * time.Minute},
 		{Name: "PT-Name-Populator", Func: populateMissingPortugueseNames, Interval: 6 * time.Hour},
-		{Name: "WoE-Char-Rankings", Func: scrapeWoeCharacterRankings, Interval: 15 * time.Minute},
+		{Name: "WoE-Char-Rankings", Func: scrapeWoeCharacterRankings, Interval: 12 * time.Hour},
 	}
 
 	// Start all standard jobs
